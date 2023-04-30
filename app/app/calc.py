22e5ca0f2a3c3e577b6caf74de4ccd5930035ac5
analyticswithrais <analyticswithrais@gmail.com>
2023-04-30 09:11:08 +0530

    Postgres SQL DB Adaptor configured

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+&Calculator functions
+"""
+
+def add(a,b):
+    """
+    &Add a and b and return result.
+      """
+    return a+b
+def subtract(a,b):
+    """
+    &Subtract a and b and return result.
+      """
+    return a-b
